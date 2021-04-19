f37b5ed87337d7a6961d4aad4e72c5e6e64a41fa
ali <aliakbari76318@gmail.com>
2021-04-19 14:24:49 +0430

    create RuleDTO.java, SectionDTO.java

---
+++ src/main/java/com/example/search/dto/RuleDTO.java
@@ -0,0 +1,17 @@
+package com.example.search.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+public class RuleDTO {
+    private String value;
+    private String field;
+    private String operator;
+    private List<String> values;//Used in case of IN operator
+}
---
+++ src/main/java/com/example/search/dto/SectionDTO.java
@@ -0,0 +1,16 @@
+package com.example.search.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+public class SectionDTO {
+    private String condition;
+    private SectionDTO section;
+    private List<RuleDTO> rules;
+}
