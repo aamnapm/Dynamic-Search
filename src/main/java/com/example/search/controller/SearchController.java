fdc864fe0529e9b44ae7c458a34d3fd9bc53b3ba
ali <aliakbari76318@gmail.com>
2021-04-20 11:40:58 +0430

    clean comment code

--- src/main/java/com/example/search/controller/SearchController.java
+++ src/main/java/com/example/search/controller/SearchController.java
@@ -2,7 +2,9 @@ package com.example.search.controller;
 
 import com.example.search.dto.SectionDTO;
 import com.example.search.mapper.SectionMapper;
+import com.example.search.model.Address;
 import com.example.search.model.Product;
+import com.example.search.service.ISearchAddressService;
 import com.example.search.service.ISearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,51 +23,16 @@ public class SearchController {
 
     private final SectionMapper sectionMapper;
     private final ISearchService iSearchService;
+    private final ISearchAddressService iSearchAddressService;
 
     @PostMapping
     public ResponseEntity<List<Product>> search(@RequestBody SectionDTO sectionDTO) {
-
-      /*  List<RuleDTO> ruleDTOS = new ArrayList<>();
-        RuleDTO ruleDTO = RuleDTO.builder()
-                .field("name")
-                .operator("LIKE")
-                .value("zara")
-                .build();
-
-        RuleDTO ruleDTO2 = RuleDTO.builder()
-                .field("name")
-                .operator("LIKE")
-                .value("Sony")
-                .build();
-
-        RuleDTO ruleDTO3 = RuleDTO.builder()
-                .field("name")
-                .operator("LIKE")
-                .value("100")
-                .build();
-
-        ruleDTOS.add(ruleDTO);
-        ruleDTOS.add(ruleDTO2);
-        ruleDTOS.add(ruleDTO3);
-
-
-        List<RuleDTO> rules2 = new ArrayList<>();
-        RuleDTO ruleDTO4 = RuleDTO.builder()
-                .field("price")
-                .operator("EQUALS")
-                .value("100")
-                .build();
-        rules2.add(ruleDTO4);
-
-        SectionDTO sectionDTO2 = SectionDTO.builder().condition("AND").rules(rules2).build();
-
-
-        SectionDTO sectionDTO1 = SectionDTO.builder().condition("OR").rules(ruleDTOS).build();
-
-//        section.setSection(section2);
-
-*/
         return new ResponseEntity<>(iSearchService.search(sectionMapper.toSection(sectionDTO)), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/a")
+    public ResponseEntity<List<Address>> searchA(@RequestBody SectionDTO sectionDTO) {
+        return new ResponseEntity<>(iSearchAddressService.searchA(sectionMapper.toSection(sectionDTO)), HttpStatus.OK);
     }
 
 }
