b41822d3024e3b53ba2dddc53c9918a570920f93
ali <aliakbari76318@gmail.com>
2021-04-21 12:13:52 +0430

    update ProductService.java and AddressService.java

---
+++ src/main/java/com/example/search/service/AddressService.java
@@ -0,0 +1,9 @@
+package com.example.search.service;
+
+import com.example.search.model.Address;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class AddressService extends SearchService<Address, String> {
+}
