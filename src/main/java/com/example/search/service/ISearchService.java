ef63d2e2d9f446a924efed7136424395b0cdbc22
ali <aliakbari76318@gmail.com>
2021-04-21 12:14:38 +0430

    update ISearchService.java

--- src/main/java/com/example/search/service/ISearchService.java
+++ src/main/java/com/example/search/service/ISearchService.java
@@ -1,12 +1,9 @@
 package com.example.search.service;
 
-import com.example.search.model.Product;
-import com.example.search.model.Section;
+import com.example.search.dto.SectionDTO;
 
 import java.util.List;
 
-public interface ISearchService {
-
-    List<Product> search(Section section);
-
+public interface ISearchService<T> {
+    List<T> search(SectionDTO section);
 }
