aa3b9ea20154a5ba512194413f66bcabeaea6f27
ali <aliakbari76318@gmail.com>
2021-04-28 13:30:12 +0430

    update AddressController.java add toDTOInfo and addressMapper

--- src/main/java/com/example/search/controller/AddressController.java
+++ src/main/java/com/example/search/controller/AddressController.java
@@ -1,16 +1,29 @@
 package com.example.search.controller;
 
+import com.example.search.dto.AddressDTO;
+import com.example.search.mapper.AddressMapper;
 import com.example.search.model.Address;
 import com.example.search.service.ISearchService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping(value = "/address")
-public class AddressController extends SearchController<Address> {
+public class AddressController extends SearchController<Address, AddressDTO.Info> {
+
+    @Autowired
+    private AddressMapper addressMapper;
 
     public AddressController(ISearchService<Address> iNICICOSearchService) {
         super(iNICICOSearchService);
+    }
+
+    @Override
+    protected List<AddressDTO.Info> toDTOInfo(List<Address> list) {
+        return addressMapper.toDTOInfo(list);
     }
 }
