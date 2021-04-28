bb1218624d00205e3ac905000a0b24233b75e232
ali <aliakbari76318@gmail.com>
2021-04-28 13:28:56 +0430

    create PersonDTO.java

---
+++ src/main/java/com/example/search/dto/PersonDTO.java
@@ -0,0 +1,48 @@
+package com.example.search.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PersonDTO {
+
+    private Integer age;
+    private String firstName;
+    private String lastName;
+    private LocalDateTime birthdate;
+
+
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    @ApiModel("PersonInfo")
+    public static class Info extends PersonDTO {
+        private Long id;
+        private List<AddressDTO> addressList;
+    }
+
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    @ApiModel("PersonCreate")
+    public static class Create extends PersonDTO {
+
+    }
+
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    @ApiModel("PersonUpdate")
+    public static class Update extends PersonDTO {
+
+    }
+}
