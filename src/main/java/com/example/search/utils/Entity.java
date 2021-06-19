54fa5768ea81e9d1f4f7e28e8897dff1ff0d44c3
ali <aliakbari76318@gmail.com>
2021-06-19 14:59:00 +0430

    create Entity.findEntityFieldsName method

--- src/main/java/com/example/search/utils/Entity.java
+++ src/main/java/com/example/search/utils/Entity.java
@@ -1,9 +1,15 @@
 package com.example.search.utils;
 
+import com.example.search.dto.FieldTypeDTO;
 import com.example.search.service.ISearchService;
 import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl;
 
+import javax.persistence.Column;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Entity {
 
@@ -24,5 +30,25 @@ public class Entity {
         }
         //                throw ;
         return null;
+    }
+
+    public List<FieldTypeDTO> findEntityFieldsName(Class clazz) {
+        List<FieldTypeDTO> fieldTypeDTOS = new ArrayList<>();
+
+        Field[] declaredFields = clazz.getDeclaredFields();
+        for (Field field : declaredFields) {
+            Annotation[] declaredAnnotations = field.getDeclaredAnnotations();
+            for (Annotation annotation : declaredAnnotations) {
+                if (annotation instanceof Column && declaredAnnotations.length == 1) {
+                    FieldTypeDTO fieldTypeDTO = new FieldTypeDTO();
+                    fieldTypeDTO.setName(field.getName());
+                    fieldTypeDTO.setType(field.getType());
+
+                    fieldTypeDTOS.add(fieldTypeDTO);
+                }
+            }
+        }
+
+        return fieldTypeDTOS;
     }
 }
