7c9a1c5ce49ba1f22a68f0ad2f91f110a42b04b0
ali <aliakbari76318@gmail.com>
2021-06-22 07:29:05 +0430

    add dual in RuleDTO.java

--- src/main/java/com/example/search/dto/RuleDTO.java
+++ src/main/java/com/example/search/dto/RuleDTO.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Builder
 public class RuleDTO {
     private String value;
+    private DualDTO dual;
     private String field;
     private String operator;
     private List<String> values;//Used in case of IN operator
